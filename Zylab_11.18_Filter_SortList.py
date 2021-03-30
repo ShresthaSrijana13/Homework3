6b7abf7c08e898588cfe12891a14077e19f4f182
sshres22 <sshres22@cougar.uh.edu>
2021-03-30 15:25:09 -0500

    Complete

--- Zylab_11.18_Filter_SortList.py
+++ Zylab_11.18_Filter_SortList.py
@@ -1,13 +1,14 @@
 # Srijana Shrestha
 # 1918305
-num1 = input()
-num2 = num1.split()
-list1 = []
 
-for i in num2:
-    if int(i) >= 0:
-        list1.append(int(i))
+num1 = input()  # user input
+num2 = num1.split()  # converting num1 into list
+list1 = []  # assigning empty list
 
-list1.sort()
-for i in list1:
-    print(i, end=' ')
+for i in num2:  # for loop
+    if int(i) >= 0:  # if condition to check whether the value is greater or qual to zero
+        list1.append(int(i))  # adding the value into list
+
+list1.sort()  # sorting the list1
+for i in list1:  # for loop iterate in list1
+    print(i, end=' ')  # printing the value with space
